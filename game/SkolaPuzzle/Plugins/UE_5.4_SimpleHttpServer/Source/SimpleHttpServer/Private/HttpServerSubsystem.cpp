8e3bf39b5f56e9b5222d1f1cbf2e6115577bbf96
StanleyJancha <136714825+StanleyJancha@users.noreply.github.com>
2024-11-22 12:34:09 +0100

    predelani computoru a smazatni http severu
    e

--- game/SkolaPuzzle/Plugins/UE_5.4_SimpleHttpServer/Source/SimpleHttpServer/Private/HttpServerSubsystem.cpp
+++
@@ -1,7 +0,0 @@
-#include "HttpServerSubsystem.h"
-#include "SimpleHttpServer.h"
-
-USimpleHttpServer* UHttpServerSubsystem::GetSimpleHttpServer(TSubclassOf<USimpleHttpServer> SimpleHttpServerClass)
-{
-    return NewObject<USimpleHttpServer>(this, SimpleHttpServerClass);
-}
